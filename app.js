856c0d79edeabda20ef47017d13c6236d6e948ec
Jeremie <jlpvmedia@gmail.com>
2023-10-19 19:10:49 +0200

    Add JS animations and responsiveness

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-function smoothScroll(){
-    document.getElementsByClassName('nav-link').scrollIntoView({
-        behavior: 'smooth'
+// Changes opacity on 'about-me' image on hover
+    //Sets the event target
+    let animationTarget = document.getElementById('about-image');
+    //Sets the event handler function when mouse hovers in
+    let animationFunctionIn = () => event.target.style.opacity = 0.5;
+    //Sets the event handler function when mouse hovers out
+    let animationFunctionOut = () => event.target.style.opacity = '';
+    //Sets the event listener - Mouse In
+    animationTarget.addEventListener('mouseenter', animationFunctionIn);
+    //Sets the event listener - Mouse Out
+    animationTarget.addEventListener('mouseleave', animationFunctionOut);
+
+// Changes opacity on 'work' images on hover
+    //Sets the event target
+    let animationTarget2 = document.getElementsByClassName('work-image');
+    const arr = Array.from(animationTarget2);
+    //Sets the event listener - Mouse In
+    arr.forEach(element => {
+        element.addEventListener('mouseenter', animationFunctionIn);
     });
-}
+    //Sets the event listener - Mouse Out
+    arr.forEach(element => {
+        element.addEventListener('mouseleave', animationFunctionOut);
+    });
